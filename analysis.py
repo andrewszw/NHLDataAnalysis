bdf6c9da1aa2c35b551f3fda580db47b9ca7162d
Zachary <zacharyandrews93@gmail.com>
2016-12-07 15:25:31 -0500

    I hope I'm done this time, for real

--- analysis.py
+++ analysis.py
@@ -1,35 +1,101 @@
-import csv
+import pymongo
 import numpy as np
 
+def team_correlation(db, boolean):
 
-def read_csv():
-    nhl_data = list()
-    with open('data.csv', 'r') as csvfile:
-        r = csv.reader(csvfile, delimiter=',')
-        next(r, None)
-        for data in r:
-            nhl_data.append(data[4:])
+    team_data = list()    
+    for item in db.hockey.find({'home': boolean}):
+        tmp_list = [
+            item['goals_for'], item['goals_against'],
+            item['shots_for'], item['shots_against'],
+            item['pp_success'], item['pp_attempt'],
+            item['pk_success'], item['pk_attempt']
+        ]
+        team_data.append(tmp_list)
+    matrix = np.array(team_data).astype(np.float)
+    team_corr = np.corrcoef(matrix.T)
+    
+    return team_corr
 
-    matrix = np.array(nhl_data).astype(np.float)
-    print(len(matrix))
 
-    cor_matrix = np.corrcoef(matrix.T)
+def average_shots_for(db):
+    home_shots_win = 0
+    home_shots_loss = 0
+    away_shots_win = 0
+    away_shots_loss = 0
+    total_home_games = 0
+    total_away_games = 0
+    for item in db.hockey.find({'home': 1}):
+        if item['goals_for'] > item['goals_against']:
+            home_shots_win += item['shots_for']
+        elif item['goals_for'] < item['goals_against']:
+            home_shots_loss += item['shots_for']
+        total_home_games += 1
 
-    return cor_matrix
+    for item in db.hockey.find({'home': 0}):
+        if item['goals_for'] > item['goals_against']:
+            away_shots_win += item['shots_for']
+        elif item['goals_for'] < item['goals_against']:
+            away_shots_loss += item['shots_for']
+        total_away_games += 1
 
-def determine_correlation(data):
+    print("Average Shots by Home Team in Win: %f" % (home_shots_win / (total_home_games / 2)))
+    print("Average Shots by Home Team in Loss: %f" % (home_shots_loss / (total_home_games / 2)))
+    print("Average Shots by Away Team in Win: %f" % (away_shots_win / (total_away_games / 2)))
+    print("Average Shots by Away in Loss: %f" % (away_shots_loss / (total_away_games / 2)))
 
-    # round all numbers in the correlation matrix to the
-    # nearest 5 decimal places.
-    correlation_data = list()
-    rounded_list = [['%.5f' % round(y, 5) for y in x] for x in data]
-    for x in rounded_list:
-        print(x)
+    total_shots = (home_shots_win + home_shots_loss) + (away_shots_win + away_shots_loss)
+    total_games = total_home_games + total_away_games
+    print("Average Shots Per Game: %f" % (total_shots / total_games))
+    
+    
+
+def average_goals_for(db):
+    home_goals_win = 0
+    home_goals_loss = 0
+    away_goals_win = 0
+    away_goals_loss = 0
+    total_home_games = 0
+    total_away_games = 0
+    for item in db.hockey.find({'home': 1}):
+        if item['goals_for'] > item['goals_against']:
+            home_goals_win += item['goals_for']
+        elif item['goals_for'] < item['goals_against']:
+            home_goals_loss += item['goals_for']
+        total_home_games += 1
+
+    for item in db.hockey.find({'home': 0}):
+        if item['goals_for'] > item['goals_against']:
+            away_goals_win += item['goals_for']
+        elif item['goals_for'] < item['goals_against']:
+            away_goals_loss += item['goals_for']
+        total_away_games += 1
+
+    print("Average Goals by Home Team in Win: %f" % (home_goals_win / (total_home_games / 2)))
+    print("Average Goals by Home Team in Loss: %f" % (home_goals_loss / (total_home_games / 2)))
+    print("Average Goals by Away Team in Win: %f" % (away_goals_win / (total_away_games / 2)))
+    print("Average Goals by Away in Loss: %f" % (away_goals_loss / (total_away_games / 2)))
+
+    total_goals = (home_goals_win + home_goals_loss) + (away_goals_win + away_goals_loss)
+    total_games = total_home_games + total_away_games
+    print("Average Goals Per Game: %f" % (total_goals / total_games))
 
 
 def main():
-    data = read_csv()
-    determine_correlation(data)
+    client = pymongo.MongoClient("localhost", 27017)
+
+    db = client.nhl
+
+    hteam_corr = team_correlation(db, 1)
+    hteam_rounded = [['%.5f' % round(y, 5) for y in x] for x in hteam_corr]
+
+    ateam_corr = team_correlation(db, 0)
+    ateam_rounded = [['%.5f' % round(y, 5) for y in x] for x in ateam_corr]
+
+    average_shots_for(db)
+    print()
+    average_goals_for(db)
+    print()
 
 
 if __name__ == '__main__':
---
+++ scrape_v3.py
@@ -0,0 +1,184 @@
+from bs4 import BeautifulSoup as bsoup
+import requests
+import csv
+
+
+# Team abbreviations to help with url building
+TEAMS = {
+    'Anaheim'      : 'ana',
+    'Arizona'      : 'ari',
+    'Boston'       : 'bos',
+    'Buffalo'      : 'buf',
+    'Calgary'      : 'cgy',
+    'Carolina'     : 'car',
+    'Chicago'      : 'chi',
+    'Colorado'     : 'col',
+    'Columbus'     : 'cbj',
+    'Dallas'       : 'dal',
+    'Detroit'      : 'det',
+    'Edmonton'     : 'edm',
+    'Florida'      : 'fla',
+    'Los Angeles'  : 'la',
+    'Minnesota'    : 'min',
+    'Montreal'     : 'mtl',
+    'Nashville'    : 'nsh',
+    'New Jersey'   : 'nj',
+    'NY Islanders' : 'nyi',
+    'NY Rangers'   : 'nyr',
+    'Ottawa'       : 'ott',
+    'Philadelphia' : 'phi',
+    'Pittsburgh'   : 'pit',
+    'San Jose'     : 'sj',
+    'St. Louis'    : 'stl',
+    'Tampa Bay'    : 'tb',
+    'Toronto'      : 'tor',
+    'Vancouver'    : 'van',
+    'Washington'   : 'wsh',
+    'Winnipeg'     : 'wpg'
+}
+
+
+def scrape_season(url, team_name, year):
+    
+    # get the soup
+    r = requests.get(url).text
+    soup = bsoup(r, 'lxml')
+
+    # list of game objects
+    game_list = list()
+
+    # find the table of games
+    table = soup.find('table', 'tablehead')
+    for row in table.find_all('tr')[1:]:    
+
+        # skip column headers
+        if dict(row.attrs)['class'][0] == 'colhead':
+            continue
+        if dict(row.attrs)['class'][0] == 'stathead':
+            continue
+
+        # get the individual cells
+        cells = row.find_all('td')
+        if len(cells) != 9:
+            continue
+
+        # get the date of the event
+        game_date = cells[0].text
+
+        # determine the home and away teams
+        home_team = 0
+        opponent_team = ''
+		
+        # ignore games against teams that no longer exist
+        try:
+            lis = cells[1].find_all('li')
+            if lis[0].text == 'vs' and len(lis) == 3:
+                opponent_team = lis[2].text
+                home_team = 1
+            elif lis[0].text == '@' and len(lis) == 3:
+                opponent_team = lis[2].text
+                home_team = 0
+            elif lis[0].text == 'vs' and len(lis) == 2:
+                opponent_team = lis[1].text
+                home_team = 1
+            elif lis[0].text == '@' and len(lis) == 2:
+                opponent_team = lis[1].text
+                home_team = 0
+        except IndexError:
+            continue
+
+        # determine the home and away team scores
+        try:
+            score = cells[2].a.text.split(' ')[0].split('-')
+        except AttributeError:
+            continue
+
+        team_score = 0;
+        opponent_team_score = 0;
+        if cells[2].find_all('li')[0].span.text == 'L':
+            team_score = int(score[1])
+            opponent_team_score = int(score[0])
+        elif cells[2].find_all('li')[0].span.text == 'W':
+            team_score = int(score[0])
+            opponent_team_score = int(score[1])
+
+        # determine shots for and against
+        try:
+            shots = cells[6].text.split('-')
+            shots_for = shots[0]
+            shots_against = shots[1]
+        except AttributeError:
+            continue
+
+        # determine powerplay success and attempts
+        try:
+            powerplay = cells[7].text.split('-')
+            powerplay_success = powerplay[0]
+            powerplay_attempts = powerplay[1]
+        except AttributeError:
+            continue
+
+        # determine penalty kill success and attempts
+        try:
+            penalty_kill = cells[8].text.split('-')
+            penalty_kill_success = penalty_kill[0]
+            penalty_kill_attempts = penalty_kill[1]
+        except AttributeError:
+            continue
+
+        
+        game_list.append([
+            team_name, opponent_team, home_team, team_score, opponent_team_score,
+            shots_for, shots_against, powerplay_success,
+            powerplay_attempts, penalty_kill_success, penalty_kill_attempts, game_date, year
+        ])
+        """game_list.append([
+            year, game_date, home_team, away_team, 
+            home_team_score, away_team_score,
+            shots_for, shots_against, powerplay_success,
+            powerplay_attempts, penalty_kill_success,
+            penalty_kill_attempts
+        ])"""
+
+    return game_list
+
+
+def compare(x, y):
+    if x[0] == y[0] and x[1] == y[1] and x[2] == y[2]:
+        return True
+    else:
+        return False
+
+
+def write_to_csv(data):
+    with open('data.csv', 'a') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        for x in data:
+            csvwriter.writerow(x)
+
+
+def main():
+    
+    sorted_teams = sorted(TEAMS.items())
+    year = input("Please enter a year: ")
+    season = list()
+    for team in sorted_teams:
+        url = 'http://www.espn.com/nhl/team/schedule/_/name/' + team[1] + '/year/' + year
+        # scrape all games of a particular season for a team
+        game_list = scrape_season(url, team[0], year)
+        season.append(game_list)
+    
+    compressed_season = [game for x in season for game in x]
+    """final_season = list()
+    for i in range(len(compressed_season)):
+        for j in range(i + 1, len(compressed_season)):
+            flag = compare(compressed_season[i], compressed_season[j])
+            if flag:
+                final_season.append(compressed_season[i])
+                break"""
+
+    write_to_csv(compressed_season)
+
+
+if __name__ == '__main__':
+    main()
